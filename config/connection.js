70c0e83577226fe320ec628dfb85b40c72507a58
Joe Basquez <jybasquez@yahoo.com>
2021-03-07 09:07:06 -0500

    connection made

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,9 @@
 const mysql = require('mysql');
+
+const connections = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '12345678',
+    database: burgers_db
+})
