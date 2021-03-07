0c5a1b4bcb9d0ee708a4dfd1a7712fcb84eb9a11
Joe Basquez <jybasquez@yahoo.com>
2021-03-07 08:52:06 -0500

    debugging

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (burger_name) VALUES ('jalapeno burger');
 INSERT INTO burgers (burger_name) VALUES ('hamburger');
-INSERT INTO burgers (burger_name) VALUES ('cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheeseburger', true);
