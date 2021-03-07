afec68817cab203e54045b7dec3926f35a2583f1
Joe Basquez <jybasquez@yahoo.com>
2021-03-07 08:45:46 -0500

    added in burgers table

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,9 @@
 CREATE DATABASE burgers_db;
 use burgers_db;
 
+CREATE TABLE burgers_db
+(
+    id int not null auto_increment primary key,
+    burger_name varchar(30) NOT NULL,
+    devoured BOOLEAN DEFAULT false
+)
