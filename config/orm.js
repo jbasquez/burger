a884b88eef06cf625bf49235600212512eabd0cd
Joe Basquez <jybasquez@yahoo.com>
2021-03-08 22:15:19 -0500

    started orm.js file made get all command

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,8 @@
 const connection = require('./connection.js');
+const burger = require('../models/burger.js');
 
+module.exports = (app) =>{
+    app.get('/api/all', (req, res) =>{
+        burger.findAll({}).then((results) =>res.json(results));
+    });
+};
