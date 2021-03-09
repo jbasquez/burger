cac16eb8a52ffb9f122ff747458275f853c908b3
Joe Basquez <jybasquez@yahoo.com>
2021-03-08 22:21:06 -0500

    added in post function to orm.js

--- config/orm.js
+++ config/orm.js
@@ -5,4 +5,11 @@ module.exports = (app) =>{
     app.get('/api/all', (req, res) =>{
         burger.findAll({}).then((results) =>res.json(results));
     });
+    app.post('/api/new', (req, res) =>{
+        console.log('rendering new data');
+        burger.create({
+            burger_name: "fds", //place hold
+            devoured: true //place holder
+        }).then((results) =>res.json(results));
+    })
 };
